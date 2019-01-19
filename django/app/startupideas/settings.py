176d29ebe2ad155451d6a69a9e29e4759757f3e1
Aaron Yong <a.yong91@gmail.com>
2019-01-18 21:34:58 -0700

    Forgot to add django_extensions to INSTALLED_APPS
    The rust is showing...

--- django/app/startupideas/settings.py
+++ django/app/startupideas/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+DJANGO_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,6 +38,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+
+THIRD_PARTY_APPS = [
+    'django_extensions'
+]
+
+CUSTOM_APPS = []
+
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + CUSTOM_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
